7554998c4fa27a9bc2506f9b7b1dbf5827c262ed
luis calvo <luis.calvo@volvocars.com>
2024-09-05 17:09:34 +0200

    123

--- function_app.py
+++ function_app.py
@@ -2,6 +2,8 @@ import logging
 import os
 import shutil
 import azure.functions as func
+import fitz  # pymupdf
+import datetime
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -35,6 +37,35 @@ def WatermarkV2(req: func.HttpRequest) -> func.HttpResponse:
                     status_code=400
                 )
 
+            # Use os.path.splitext() to get file name and extension
+            file_name = os.path.splitext(os.path.basename(PDF_Path))[0]
+            logging.info(f"Processing file: {file_name}")
+
+            # Open PDF via PyMuPDF
+            doc = fitz.open(PDF_Path)
+            # Get current time
+            now = datetime.datetime.now()
+            # Transform to friendly string
+            formatted_date = now.strftime("%Y/%#m/%#d %H:%M:%S")
+            page = doc[0]  # new page, or choose doc[n]
+            # The text strings, each having 3 lines
+            remark_text = f"Confirmed by: {Approver}\nConfirm time: {formatted_date}"
+            # Get center of the page
+            center_x = page.rect.width / 2
+            center_y = page.rect.height - 50  # Place the text at the bottom of the page, leaving some whitespace
+            p1 = fitz.Point(center_x - 40, center_y + 25)
+
+            # Create a Shape to draw on
+            shape = page.new_shape()
+            # Insert the text strings
+            shape.insert_text(p1, remark_text, color=(1, 0, 0), fontname="helv")
+            # Store our work to the page
+            shape.commit()
+
+            # Output modified PDF
+            output_pdf_path = os.path.join(Out_Path, f"{file_name}_added.pdf")
+            doc.save(output_pdf_path)
+
             # Check if the output directory exists, if not, create it
             if not os.path.exists(Out_Path):
                 try:
@@ -46,21 +77,7 @@ def WatermarkV2(req: func.HttpRequest) -> func.HttpResponse:
                         status_code=500
                     )
 
-            # Define the output file name (same as input)
-            file_name = os.path.basename(PDF_Path)
-            out_file_path = os.path.join(Out_Path, file_name)
-
-            # Copy the file from input path to output path
-            try:
-                shutil.copy2(PDF_Path, out_file_path)
-            except Exception as e:
-                logging.error(f"Error copying file: {str(e)}")
-                return func.HttpResponse(
-                    f"Error copying file: {str(e)}",
-                    status_code=500
-                )
-
-            return func.HttpResponse(f"PDF file processed and saved to {out_file_path}", status_code=200)
+            return func.HttpResponse(f"PDF file processed and saved to {output_pdf_path}", status_code=200)
 
         except Exception as e:
             logging.error(f"Unexpected error: {str(e)}")
